beff73289cd349933e427732740b2867fbeca59f
rahul2103 <rahulsingh2103@yahoo.com>
2024-09-24 18:50:29 +0530

    publisher

--- forge.config.js
+++ forge.config.js
@@ -41,4 +41,17 @@ module.exports = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'rahul2103',
+          name: 'electron',
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ],
 };
