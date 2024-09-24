9e54e03eb0bda89f760564d3537f0247b772f63c
rahul2103 <rahulsingh2103@yahoo.com>
2024-09-24 16:52:24 +0530

    ipcRenderer

--- renderer.js
+++ renderer.js
@@ -3,7 +3,6 @@ information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.
 
 const func = async () => {
   const response = await window.versions.ping()
-  console.log(response) // prints out 'pong'
 
   const ipcinfo = document.getElementById('ipcinfo')
   ipcinfo.innerText = `response: ${response}`
